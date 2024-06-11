12c9184b92ba9d6d90fdfbc64f8e193ac5341daa
Megami.ay <megami.ay@gmail.com>
2024-06-11 09:40:57 -0300

    Update agendaC.c

--- agendaC.c
+++ agendaC.c
@@ -1,6 +1,8 @@
-//Adicionar um contato
 #include <stdio.h>
 #include <stdlib.h>
+
+// prototipagem de funcs aqui em cima
+
 
 int main() {
     char nome[50];
@@ -21,6 +23,46 @@ int main() {
     return 0;
 }
 
+// Não sei se pode functions (se não puder a gente deleta tudo)
+// acho que fica mais fácil se a gente fizer elas separadas e só juntas tudo no main
+
+// int add(){
+    
+// }
+
+// int consul(){
+    
+// }
+
+// int rm(){
+    
+// }
+
+// int menu(){
+//  int op;
+    
+//  printf("Escolha qual vai ser sua acao nos contatos:\n 1. Adicionar\n 2. Pesquisar\n 3. Remover\n 4. Sair");
+//  scanf("%i", &op);
+    
+//  switch(op){
+//      case 1:
+          // puxa a func de adição
+//          break;
+//      case 2:
+          // puxa a func de consulta
+//          break;
+//      case 3:
+          // puxa a func de remoção
+//          break;
+//      case 4:
+          //encerra o programa
+//      default:
+//          printf("Esta opcao nao existe");
+//          break;
+//  }
+    
+// }
+
 //Consultar um contato (listar/ler)
 
 //Remover um contato
@@ -28,3 +70,4 @@ int main() {
 //Armazenar contatos (código binário)
 
 //Menu (escolha: fazer, pesquisar, remover, sair)
+}
