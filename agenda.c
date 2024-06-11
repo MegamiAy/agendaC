2a427cb61b44447df1ecf23b8600d430e378092e
gabilbck <gabrielieduardalembeck@gmail.com>
2024-06-11 15:05:51 -0300

    um detalhe

--- agenda.c
+++ agenda.c
@@ -73,7 +73,7 @@ int consul(){
     fscanf(fptr, "%s", phone);
     printf("Nome: %s\n", nome);
     printf("Telefone: %s\n\n", phone);
-    fclose(fptr);4
+    fclose(fptr);
     return 0;
 }
         
